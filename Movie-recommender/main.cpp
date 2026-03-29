632f97269d3d407474b586e319317fb1734b9f1a
Jin-uu <jinu200131@gmail.com>
2026-03-29 18:35:49 +0900

    refactor: change test

--- Movie-recommender/main.cpp
+++ Movie-recommender/main.cpp
@@ -7,7 +7,7 @@
 #include "Rating.h"
 
 void TestMovie() {
-  std::cout << "=== Test Movie Class ===" << std::endl;
+  std::cout << "============ Test Movie Class ============" << std::endl;
 
   Movie m1(1, "기생충", "드라마", 2019);
   Movie m2(2, "인터스텔라", "SF", 2014);
@@ -31,36 +31,32 @@ void TestMovie() {
 }
 
 void TestUser() {
-  std::cout << "=== Test User Class ===" << std::endl;
+  std::cout << "============ Test User Class ============" << std::endl;
   User user_1(1, "박진우", "jinu@example.com");
   User user_2(2, "홍길동", "hong@example.com");
-  user_1.Display();
-  user_2.Display();
 
   std::vector<User> users;
   users.push_back(user_1);
   users.push_back(user_2);
   
   for (const auto& user : users) {
-    std::cout << user.name() << ": " << user.email() << std::endl;
+    user.Display();
   }
 }
 
 void TestRating() {
-  std::cout << "=== Test Rating Class ===" << std::endl;
+  std::cout << "============ Test Rating Class ============" << std::endl;
   Rating rating_1(1, 1, 4.8);
-  Rating rating_2(1, 2, 9.9); // 유효성 검사로 score 0.0으로 설정
-  rating_1.Display();
-  rating_2.Display();
+  Rating rating_2(1, 2, 9.9);  // 유효성 검사로 score 0.0으로 설정
+  Rating rating_3(2, 1, -1.2); // 유효성 검사로 score 0.0으로 설정
 
   std::vector<Rating> ratings;
   ratings.push_back(rating_1);
   ratings.push_back(rating_2);
+  ratings.push_back(rating_3);
 
   for (const auto& rating : ratings) {
-    std::cout << "User ID: " << rating.user_id() 
-              << ", Movie ID: " << rating.movie_id() 
-              << ", Score: " << rating.score() << std::endl;
+    rating.Display();
   }
 }
 
